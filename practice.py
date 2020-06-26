73ebc783e304a25a687ac4629cdd58920a459178
sereen-ba <64044506+sereen-ba@users.noreply.github.com>
2020-06-26 22:20:12 +0300

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,6 @@
+# you can use this code to generates new color from the BGR set 
+# in this file we are using Trackbar method and displaying our changes 
+
 import cv2 as cv
 import numpy as np
 
@@ -5,7 +8,7 @@ def nothing(x):
     print(x)
 
     
-img= np.zeros((300,512,3),np.uint8)
+img= np.zeros((300,300,3),np.uint8)
 cv.namedWindow('control', 0)
 
 cv.createTrackbar('B','control',0,255,nothing)
@@ -27,21 +30,5 @@ while(1):
     elif s==1:
         img[:] = [b,g,r]
 
-
-
-
-#relation operation with pixels
-#img1= np.zeros((250,500,3),np.uint8)
-#img1=cv2.rectangle(img1,(200,0),(300,100),(255,255,255),-1)
-#img2=np.zeros((250,500,3),np.uint8)
-#img2=cv2.rectangle(img2,(250,0),(500,250),(255,255,255),-1)
-#bitAnd=cv2.bitwise_and(img2,img1)
-#bitOr=cv2.bitwise_or(img2,img1)
-#bitXor=cv2.bitwise_xor(img2,img1)  
-#cv2.imshow('first',img1)
-#cv2.imshow('second',img2)
-#cv2.imshow('bitOr',bitOr)
-#cv2.imshow('bitAnd',bitAnd)
-#cv2.imshow('bitOr',bitXor)
-
 cv.destroyAllWindows()
+# Arthur:sereen bahdad
